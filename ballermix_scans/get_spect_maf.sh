15a4300b22e1a0f9c2c4318f9cd7a2d32572a98e
bfraser-commits <63240617+bfraser-commits@users.noreply.github.com>
2025-02-11 15:21:27 +0000

    Add files via upload

---
+++ ballermix_scans/get_spect_maf.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --time=02:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1 
+#SBATCH -A Research_Project-T109423
+#SBATCH -p pq
+#SBATCH --job-name=ballermix
+#SBATCH --error=ballermix.err.txt
+#SBATCH --output=ballermix.out.txt
+#SBATCH --export=All
+#SBATCH -D .
+
+module load SciPy-bundle/2020.03-foss-2020a-Python-3.8.2
+INPUT_DIR=/gpfs/ts0/projects/Research_Project-T109389/people/josie/NFDS_analysis/ballermix_holi11/outputs/formatted_freqs
+
+#loop for all pops
+pop_array=(APHP APLP ECHP ECLP GHP GLP MHP MLP P TUHP TULP)
+
+for POP in "${pop_array[@]}"
+do
+
+
+#concat all pop files without header line
+awk 'FNR>1' $INPUT_DIR/chr*_${POP}.derived.frq.derived > ${POP}_concat_derived_frq.txt
+awk 'BEGIN {print "phsPos\tgenPos\tx\tn"} {print}' ${POP}_concat_derived_frq.txt > temp.txt && mv temp.txt ${POP}_concat_derived_frq.txt
+
+#get spect
+python /lustre/home/bf299/BallerMixPlus/BalLeRMix+_v1.py --getSpect --MAF --input ${POP}_concat_derived_frq.txt --spect ${POP}_maf_spect.txt
+
+done
